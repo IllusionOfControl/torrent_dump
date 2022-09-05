ba7586d6fd7381670042a48114762016b452e929
Illusion of control <sergeyskorokhod2@gmail.com>
2022-09-05 16:10:51 +0300

    added search refactored torrent list

--- app/views/torrents.py
+++ app/views/torrents.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for, send_file, current_app, abort
+from flask import Blueprint, render_template, redirect, url_for, send_file, current_app, abort, request
 from app.models import Torrent, File
 import os
 
@@ -15,8 +15,9 @@ def index():
 @blueprint.get("/torrents/")
 @blueprint.get("/torrents/<int:page>")
 def retrieve_all_torrents(page=1):
-    torrents = Torrent.query.order_by(Torrent.id.desc()).paginate(page, 30, False)
-    return render_template("torrents_list.html", torrents=torrents)
+    pagination = Torrent.query.order_by(Torrent.id.desc()).paginate(page, 30, False)
+    torrents = pagination.items
+    return render_template("torrents_list.html", torrents=torrents, pagination=pagination)
 
 
 @blueprint.get("/torrent/<string:uid>")
@@ -42,3 +43,19 @@ def retrieve_torrent_file(uid):
         return send_file(file_path, download_name=torrent.name + ".torrent")
 
     abort(404)
+
+
+@blueprint.get("/torrent/search")
+def retrieve_search_form():
+    return render_template("search_form.html")
+
+
+@blueprint.get("/torrent/search/")
+@blueprint.get("/torrent/search/<int:page>")
+def search_by_torrents_name(page=1):
+    search_query = request.args.get("query", None)
+    print(search_query)
+    if not search_query:
+        abort(403)
+    torrents = Torrent.query.filter(Torrent.name.like(f"%{search_query}%")).order_by(Torrent.id.desc()).all()
+    return render_template("torrents_list.html", torrents=torrents)
